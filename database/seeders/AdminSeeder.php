080da3cfc65eccc9c4a4a4c61bfc120f9750fb70
FeniksZharovik <158120171+FeniksZharovik@users.noreply.github.com>
2026-02-15 14:18:05 +0700

    Delete database migration users. A database migration ALL & database admin seeder. Modified database seeder.

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin PSM',
+            'email' => 'admin@psm.com',
+            'password' => Hash::make('PSM_Pass30rd'),
+        ]);
+    }
+}
