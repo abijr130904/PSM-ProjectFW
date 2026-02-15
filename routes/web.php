5094b3b59fe003e5767dfb4d5cdb015e23f7551c
FeniksZharovik <158120171+FeniksZharovik@users.noreply.github.com>
2026-02-15 14:19:16 +0700

    Modified routes web

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Pages\ArticleController;
+use App\Http\Controllers\Pages\ProgramController;
+use App\Http\Controllers\Pages\MemberController;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Public Routes (Guest / Pembaca)
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::view('/tentang', 'pages.tentang')->name('tentang');
+
+Route::get('/program', [ProgramController::class, 'index'])->name('program');
+
+Route::get('/organisasi', [MemberController::class, 'index'])->name('organisasi');
+
+Route::get('/artikel', [ArticleController::class, 'index'])
+    ->name('artikel.index');
+
+Route::get('/artikel/{slug}', [ArticleController::class, 'show'])
+    ->name('artikel.show');
