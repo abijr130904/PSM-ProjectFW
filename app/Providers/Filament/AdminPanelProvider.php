f18fccf782900cdc48e4addc442c42c6425b8f49
FeniksZharovik <158120171+FeniksZharovik@users.noreply.github.com>
2026-02-16 11:44:45 +0700

    Modified admin panel provider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->favicon(asset('assets/IC-PSM.jpg'))
             ->colors([
                 'primary' => Color::Amber,
             ])
